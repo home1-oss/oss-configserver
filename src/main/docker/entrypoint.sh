e2a3a47ac8e69f68e99e5838c2c70017122c9200
Haolun <chshawkn@hotmail.com>
2017-07-08 03:07:27 +0800

    update ci scripts

--- src/main/docker/entrypoint.sh
+++ src/main/docker/entrypoint.sh
@@ -21,3 +21,36 @@ elif [ "${1:0:1}" != '/' ]; then
 fi
 
 exec "$@"
+
+## arguments: target
+## returns: git_deploy_key_file
+#git_deploy_key_file() {
+#    local archive=$(ls | grep .jar | grep -v sources.jar | grep -v jar.original)
+#    local target=$1
+#    local result=""
+#    if [ -z "${SPRING_CLOUD_CONFIG_SERVER_DEPLOYKEY}" ]; then
+#        extract_file_from_archive ${archive} BOOT-INF/classes/deploy_key.pub ${target}
+#        if [ -f ${target} ]; then
+#            result=${target}
+#        else
+#            result=""
+#        fi
+#    elif [ ${SPRING_CLOUD_CONFIG_SERVER_DEPLOYKEY:0:10} == classpath: ]; then
+#        extract_file_from_archive ${archive} BOOT-INF/classes/${SPRING_CLOUD_CONFIG_SERVER_DEPLOYKEY:10}.pub ${target}
+#        if [ -f ${target} ]; then
+#            result=${target}
+#        else
+#            result=""
+#        fi
+#    elif [ ${SPRING_CLOUD_CONFIG_SERVER_DEPLOYKEY:0:5} == file: ] && [ -f ${SPRING_CLOUD_CONFIG_SERVER_DEPLOYKEY:0:5} ]; then
+#        result=${SPRING_CLOUD_CONFIG_SERVER_DEPLOYKEY:5}
+#    elif [ -f ${SPRING_CLOUD_CONFIG_SERVER_DEPLOYKEY} ]; then
+#        result=${SPRING_CLOUD_CONFIG_SERVER_DEPLOYKEY}
+#    else
+#        result="";
+#    fi
+#    if [ -f ${result} ]; then
+#        chmod 600 ${result}
+#    fi
+#    echo ${result}
+#}
